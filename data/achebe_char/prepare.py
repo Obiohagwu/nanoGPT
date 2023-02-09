8fb8bf634dc8d40164c0a4f0e689d078e48965b5
Obi Ohagwu <obiohagwu8@gmail.com>
2023-02-09 16:17:54 -0500

    fix file read

--- data/achebe_char/prepare.py
+++ data/achebe_char/prepare.py
@@ -10,11 +10,7 @@ import requests
 import numpy as np
 
 # download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_path = 'full_text_things-fall-apart.txt'
-    with open(input_file_path, 'w') as f:
-        f.write(requests.get(data_path).text)
+input_file_path = 'full_text_things-fall-apart.txt'
 
 with open(input_file_path, 'r') as f:
     data = f.read()
